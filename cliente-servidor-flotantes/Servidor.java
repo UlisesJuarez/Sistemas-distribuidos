faecd8ef14f628328bed64a1c4881ebd023aecbc
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-08 23:17:45 -0600

    esperando al cliente

--- cliente-servidor-flotantes/Servidor.java
+++ cliente-servidor-flotantes/Servidor.java
@@ -2,31 +2,43 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 class Servidor{
     public static void main(String[] args) {
-        try {
-            ServerSocket servidor=new ServerSocket(50000);
-            Socket conexion=servidor.accept();
+        Socket conexion=null;
+        for(;;)
+            try {
+                ServerSocket servidor=new ServerSocket(50000);
+                conexion=servidor.accept();
 
-            DataOutputStream salida=new DataOutputStream(conexion.getOutputStream());
-            DataInputStream entrada=new DataInputStream(conexion.getInputStream());     
-            
-            int n=entrada.readInt();
-            System.out.println(n);
+                DataOutputStream salida=new DataOutputStream(conexion.getOutputStream());
+                DataInputStream entrada=new DataInputStream(conexion.getInputStream());     
+                
+                int n=entrada.readInt();
+                System.out.println(n);
 
-            double x=entrada.readDouble();
-            System.out.println(x);
+                double x=entrada.readDouble();
+                System.out.println(x);
 
-            byte[] buffer=new byte[4];
-            read(entrada,buffer,0,4);
-            System.out.println(new String(buffer,"UTF-8"));
+                byte[] buffer=new byte[4];
+                read(entrada,buffer,0,4);
+                System.out.println(new String(buffer,"UTF-8"));
 
-            salida.write("HOLA".getBytes());
-            
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+                salida.write("HOLA".getBytes());
+                
+                byte[] a =new byte[5*8];
+                read(entrada, a,0,5*8);
+
+                ByteBuffer b=ByteBuffer.wrap(a);
+
+                for(int i=0; i<5; i++)System.out.println(b.getDouble());
+                conexion.close();
+                servidor.close();
+                break;
+            } catch (Exception e) {
+                for (int i = 0; i < 100; i++);
+            }
         
     }
     static void read(DataInputStream f,byte[] b,int posicion, int longitud) throws Exception
