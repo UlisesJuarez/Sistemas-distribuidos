a246fdbb1433835b6b3609647619ce0e44524e11
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-08 22:45:31 -0600

    enviando datos y cerrando el socket

--- cliente-servidor-flotantes/Cliente.java
+++ cliente-servidor-flotantes/Cliente.java
@@ -1,6 +1,7 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 class Main{
     public static void main(String[] args) {
@@ -14,14 +15,36 @@ class Main{
             salida.write("hola".getBytes());
 
             byte[] buffer =new byte[4];
-            entrada.read(buffer,0,4);
+            read(entrada,buffer,0,4);
             System.out.println(new String(buffer,"UTF-8"));
+
+            ByteBuffer b=ByteBuffer.allocate(5*8);
+            b.putDouble(1.1);
+            b.putDouble(1.2);
+            b.putDouble(1,3);
+            b.putDouble(1.4);
+            b.putDouble(1.5);
+
+            byte[] a=b.array();
+            salida.write(a);
+            Thread.sleep(1000);
+            
+            conexion.close();
             
         } catch (Exception e) {
             System.out.println(e);
+        }        
+    }
+    static void read(DataInputStream f,byte[] b,int posicion, int longitud) throws Exception
+    {
+        while(longitud>0){
+            try {
+                int n=f.read(b,posicion,longitud);
+                posicion +=n;
+                longitud -=n;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
-
-        
-        
     }
 }
