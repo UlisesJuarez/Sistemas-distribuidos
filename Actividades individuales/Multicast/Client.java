e311f98325642936293bba308c93347f3718edab
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-25 19:49:20 -0600

    actualizacion para el jdk 14

--- Actividades individuales/Multicast/Client.java
+++ Actividades individuales/Multicast/Client.java
@@ -2,14 +2,36 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.nio.ByteBuffer;
 
 public class Client {
 
     public static void main(String[] args) throws Exception {
         try {
 
-            
+            System.setProperty("java.net.preferIPv4Stack","true");
+
+
+            MulticastSocket socket= new MulticastSocket(50000);
+            InetSocketAddress grupo= new InetSocketAddress(InetAddress.getByName("230.0.0.0"),50000);
+            NetworkInterface netInter=NetworkInterface.getByName("em1");
+            socket.joinGroup(grupo, netInter);
+
+            byte a=recibe_mensaje(socket,4);
+            System.out.println(new String(a,"UTF-8"));
+
+            byte[] buffer=recibe_mensaje(socket,5*8);
+            ByteBuffer b=ByteBuffer.wrap(buffer);
+
+            for (int i = 0; i <5; i++) {
+                System.out.println(b.getDouble());
+            }
+
+            socket.leaveGroup(grupo, netInter);
+            socket.close();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
