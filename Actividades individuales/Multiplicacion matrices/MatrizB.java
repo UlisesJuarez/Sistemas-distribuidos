8903c386e020d9e8594aa81ad01ed507bdb7594b
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-25 20:57:46 -0600

    comparación de los programas terminada

--- Actividades individuales/Multiplicacion matrices/MatrizB.java
+++ Actividades individuales/Multiplicacion matrices/MatrizB.java
@@ -1,3 +1,43 @@
 public class MatrizB {
-    
+    static int N = 1000;
+    static int[][] A = new int[N][N];
+    static int[][] B = new int[N][N];
+    static int[][] C = new int[N][N];
+
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+
+        // inicializa las matrices A y B
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] = 2 * i - j;
+                B[i][j] = i + 2 * j;
+                C[i][j] = 0;
+            }
+        }
+
+        // transponiendo la matriz B, la traspuesta queda en B
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int x = B[i][j];
+                B[i][j] = B[i][j];
+                B[j][i] = x;
+            }
+        }
+
+        // multiplica la matriz A y la matriz B, el resultado se queda en la matriz C
+        //los indices de la matriz B se intercambiaron con respecto al primer programa
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    C[i][j] += A[i][k] * B[j][k];
+                }
+            }
+        }
+
+        long t2=System.currentTimeMillis();
+        System.out.println("Tiempo: "+(t2-t1)+"ms");
+
+    }
 }
