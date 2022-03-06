bf99ac42b471e962e0d86353a80c8a3bdc8869c1
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-03-06 00:48:05 -0600

    creacion de la función para el checksum

---
+++ Tareas/Tarea 3/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Matriz mat1=new Matriz();
+        mat1.calcula_checksum(8);
+        mat1.calcula_checksum(1000);
+    }
+}
